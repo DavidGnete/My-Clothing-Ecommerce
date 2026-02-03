d60862843dbefd00d09ca5db6a1191d49d7509d3
David Agudelo Ocampo <agudeloocampodavid@gmail.com>
2026-02-03 12:35:43 -0500

    update perfil images and deletes

--- src/app/(shop)/admin/product/[slug]/ui/ProductForm.tsx
+++ src/app/(shop)/admin/product/[slug]/ui/ProductForm.tsx
@@ -1,28 +1,73 @@
 "use client";
 
-import { Product } from "@/interfaces";
-import { Category } from "@prisma/client";
+import {useForm} from 'react-hook-form'
+
+import { Product,ProductImage, Category } from "@/interfaces";
+import Image from 'next/image';
+
+
 
 interface Props {
-  product: Product;
+  product: Product & {ProductImage?: ProductImage[]};
   categories: Category[];
 }
 
 const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
 
+interface FormInputs {
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  inStrock: number;
+  sizes: string[];
+  tags: string;
+  gender: 'men' | 'women' | 'kid' | 'unisex';
+  categoryId: string;
+
+  // todo: Images
+}
+
+
+
 export const ProductForm = ({ product, categories }: Props) => {
+
+  const {
+    handleSubmit,
+    register,
+    formState: {isValid},
+  } = useForm<FormInputs>({
+    defaultValues: {
+      ...product,
+      tags: product.tags.join(', '),
+      sizes: product.Size ?? [],
+
+
+      // Todo: images
+    }
+  });
+
+  const onSubmit = async (data: FormInputs) => {
+    console.log({data});
+
+
+
+  }
+
+
+
   return (
-    <form className="grid px-5 mb-16 grid-cols-1 sm:px-0 sm:grid-cols-2 gap-3">
+    <form onSubmit={handleSubmit(onSubmit)} className="grid px-5 mb-16 grid-cols-1 sm:px-0 sm:grid-cols-2 gap-3">
       {/* Textos */}
       <div className="w-full">
         <div className="flex flex-col mb-2">
           <span>Título</span>
-          <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          <input type="text" className="p-2 border rounded-md bg-gray-200"{...register('title', {required: true})} />
         </div>
 
         <div className="flex flex-col mb-2">
           <span>Slug</span>
-          <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          <input type="text" className="p-2 border rounded-md bg-gray-200" {...register('slug', {required: true})}/>
         </div>
 
         <div className="flex flex-col mb-2">
@@ -30,22 +75,24 @@ export const ProductForm = ({ product, categories }: Props) => {
           <textarea
             rows={5}
             className="p-2 border rounded-md bg-gray-200"
+            {...register('description', {required: true})}
           ></textarea>
         </div>
 
         <div className="flex flex-col mb-2">
           <span>Price</span>
-          <input type="number" className="p-2 border rounded-md bg-gray-200" />
+          <input type="number" className="p-2 border rounded-md bg-gray-200" {...register('price', {required: true,min: 0})}/>
         </div>
 
         <div className="flex flex-col mb-2">
           <span>Tags</span>
-          <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          <input type="text" className="p-2 border rounded-md bg-gray-200" 
+          {...register('tags', {required: true})}/>
         </div>
 
         <div className="flex flex-col mb-2">
           <span>Gender</span>
-          <select className="p-2 border rounded-md bg-gray-200">
+          <select className="p-2 border rounded-md bg-gray-200" {...register('gender', {required: true})}>
             <option value="">[Seleccione]</option>
             <option value="men">Men</option>
             <option value="women">Women</option>
@@ -56,7 +103,7 @@ export const ProductForm = ({ product, categories }: Props) => {
 
         <div className="flex flex-col mb-2">
           <span>Categoria</span>
-          <select className="p-2 border rounded-md bg-gray-200">
+          <select className="p-2 border rounded-md bg-gray-200" {...register('categoryId', {required: true})}>
             <option value="">[Seleccione]</option>
             {
                 categories.map( category => (
@@ -101,6 +148,40 @@ export const ProductForm = ({ product, categories }: Props) => {
               className="p-2 border rounded-md bg-gray-200" 
               accept="image/png, image/jpeg"
             />
+
+          </div>
+
+          <div className='grid grid-cols-1 sm:grid-cols-3 gap-3'>
+
+            {
+
+              product.ProductImage?.map( image => (
+
+                <div key={image.id}>
+
+                  <Image 
+                  alt={product.title ?? '' }
+                  src={`/products/${ image.url}`}
+                  width={400}
+                  height={300}
+                  className='rounded-t shadow-md'
+                  />
+
+                  <button 
+                  type="button"
+                  onClick={() => console.log(image.id, image.url)}
+                  className='btn-danger w-full rounded-b-xl'>
+                    eliminar
+                  </button>
+
+                </div>
+
+
+
+              ))
+            }
+
+
 
           </div>
 
--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -23,6 +23,12 @@ export interface CartProduct {
 }
 
 
+export interface ProductImage {
+    id: number;
+    url: string;
+    productId?: string;
+}
+
 
 export type ValidCategorie ='men'|'women'|'kid'|'unisex';
 export type Size = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
