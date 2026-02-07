74b4084701344803dd22556986d39c4b1372ba32
david <agudeloocampodavid@gmail.com>
2026-02-07 09:22:50 -0500

    improve1

--- src/components/product/slideshow/ProductMovileSlideShop.tsx
+++ src/components/product/slideshow/ProductMovileSlideShop.tsx
@@ -47,7 +47,7 @@ export const ProductMovileSlideShop = ({images, title, clasName}: Props) => {
                 <Image loading='lazy'
                 width={600}
                 height={500}
-                src={`${image }`}
+                src={image }
                 alt={title}
                 className=" object-fill"
                 />
