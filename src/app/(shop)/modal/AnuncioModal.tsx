93a3dddbf63091aa3cd6bb28d3651d3a7dd7368c
david <agudeloocampodavid@gmail.com>
2026-02-15 15:46:25 -0500

    click modal

--- src/app/(shop)/modal/AnuncioModal.tsx
+++ src/app/(shop)/modal/AnuncioModal.tsx
@@ -25,7 +25,7 @@ export const AnuncioModal = () => {
     if (!siModal) return null;
 
   return (
-    <div className="fixed  inset-0 bg-black/90 flex justify-center items-center z-50">
+    <div className="fixed  inset-0 bg-black/90 flex justify-center items-center z-50" onClick={closeModal}>
         <div className='relative w-[90%] max-w-xs 
                 md:max-w-xs
                 lg:max-w-xs
