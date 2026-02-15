f1dab86b3fbf0cdfd6c9ba899ac80e83ce583312
david <agudeloocampodavid@gmail.com>
2026-02-14 21:55:03 -0500

    center text

--- src/components/ui/title/Title.tsx
+++ src/components/ui/title/Title.tsx
@@ -9,14 +9,14 @@ interface Props {
 
 export const Title = ({title, subtitle, className}:Props ) => {
   return (
-    <div className={`mt-10 mb-0 content-center ${className}`}>
+    <div className={`flex content-center mt-10 mb-0 ${className}`}>
     <h1 className={`${titleFont.className} antialiased text-4xl font-semibold my-10`}>
         {title}
     </h1>
 
     {
         subtitle && (
-            <h3 className="text-xl mb-0">{subtitle} </h3>
+            <h3 className="text-xl mb-0 ">{subtitle} </h3>
             
         )
         
